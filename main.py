a383d81939747b148e6d2f6eabb398c50624954e
Alex-bit-collab-2019 <57102621+Alex-bit-collab-2019@users.noreply.github.com>
2021-05-05 23:37:37 +0300

    fix NoneType attribute 'center', ball:pong_ball

--- main.py
+++ main.py
@@ -7,6 +7,19 @@ from kivy.vector import Vector
 from kivy.clock import Clock
 from random import randint
 
+class PongPaddle(Widget):
+    score = NumericProperty(0)  ## очки игрока
+
+    ## Отскок мячика при коллизии с панелькой игрока
+    def bounce_ball(self, ball):
+        if self.collide_widget(ball):
+            vx, vy = ball.velocity
+            offset = (ball.center_y - self.center_y) / (self.height / 2)
+            bounced = Vector(-1 * vx, vy)
+            vel = bounced * 1.1
+
+            ball.velocity = vel.x, vel.y + offset
+
 class PongBall(Widget):
     # Скорость движения нашего шарика по двум осям
     velocity_x = NumericProperty(0)
@@ -21,6 +34,8 @@ class PongBall(Widget):
 
 class PongGame(Widget):
     ball = ObjectProperty(None)  # это будет наша связь с объектом шарика
+    player1 = ObjectProperty(None)  # Игрок 1
+    player2 = ObjectProperty(None)  # Игрок 2
 
     def serve_ball(self, vel=(4, 0)):
         self.ball.center = self.center
@@ -28,6 +43,10 @@ class PongGame(Widget):
 
     def update(self, dt):
         self.ball.move()  # двигаем шарик в каждом обновлении экрана
+
+        # проверка отскока шарика от панелек игроков
+        self.player1.bounce_ball(self.ball)
+        self.player2.bounce_ball(self.ball)
 
         # отскок шарика по оси Y
         if (self.ball.y < 0) or (self.ball.top > self.height):
